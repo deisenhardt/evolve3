cea96d1ac51e81c2ba8b3afa426c3b809ce7b50c
W4DPE <150026567+deisenhardt@users.noreply.github.com>
2025-05-24 23:09:13 -0400

    Update newinstall.sh

--- newinstall.sh
+++ newinstall.sh
@@ -5,7 +5,7 @@
 usage(){
   printf " Usage : newinstall.sh will install a set group of core packages to make desktop use more pleasant. \
         \n With the following flags, additional flags & packages can also be installed. \
-        \n\n\t -e install the repo & wireless driver for the rtl8723du \
+        \n\n\t -m install wireless driver for the rtl8723du on Maestro Evolve III \
         \n\t -r install hamradio-all \
         \n\t -g install Google Chrome \
         \n\t -d install Discord \
@@ -27,49 +27,40 @@ sudo_check(){
 
 input_check() { # check for flags
 
-    echo "$1" | grep -i "e" && evolve=1
+    echo "$1" | grep -i "m" && evolve=1
     echo "$1" | grep -i "r" && radio=1   && list+=" hamradio-all"
     echo "$1" | grep -i "g" && chrome=1  && list+=" google-chrome-stable"
     echo "$1" | grep -i "d" && discord=1 && list+=" discord"
     echo "$1" | grep -i "x" && xfce=1    && list+=" xfce4 xfce4-goodies xfwm4 xfce4-whiskermenu-plugin"
     echo "$1" | grep -i "h" && help=1    && usage
 
-  #if [[ "$help" -eq 1 ]]; then
-  #  usage
-  #fi
-
-  #if [[ "$radio" -eq 1 ]]; then
-  #  list+=" hamradio-all"
-  #fi
-
-  #if [[ "$chrome" -eq 1 ]]; then
-  #  list+=" google-chrome-stable"
-  #fi
-
-  #if [[ "$discord" -eq 1 ]]; then
-  #  list+=" discord"
-  #fi
-
-  #if [[ "$xfce" -eq 1 ]]; then
-  #  list+=" xfce4 xfce4-goodies xfwm4 xfce4-whiskermenu-plugin"
-  #fi
 }
 
 maestro_evolve() { # Commands to install new drivers (make sure secure boot is disabled):
 
-  # ubuntu PPA
-  #sudo add-apt-repository ppa:kelebek333/kablosuz
-  #sudo apt-get update
-  #sudo apt-get install -y rtl8723du-dkms
-  #sudo reboot now
+  osrelease=/etc/os-release
+  grep -qi manjaro $osrelease && distro=manjaro
+  grep -qi "Pop\!_OS" $osrelease && distro=pop
+  grep -qi ubuntu $osrelease && distro=ubuntu
+  grep -qi debian $osrelease && [ -z "$distro" ] && distro=debian
 
-  # debian
-  sudo apt-get -y install linux-headers-$(uname -r) build-essential git
-  git clone https://github.com/lwfinger/rtw88
-  cd rtw88
-  make
-  sudo make install
-  sudo make install_fw
+  case "$distro" in
+  ubuntu) 
+    sudo add-apt-repository ppa:kelebek333/kablosuz
+    sudo apt-get update
+    sudo apt-get install -y rtl8723du-dkms
+  ;;
+
+
+  debian)
+    sudo apt-get -y install linux-headers-$(uname -r) build-essential git
+    git clone https://github.com/lwfinger/rtw88
+    cd rtw88
+    make
+    sudo make install
+    sudo make install_fw
+  ;;
+  sudo reboot now
 }
 
 list="zsh nala terminator notepadqq tuxcmd doublecmd-gtk mc atop btop htop caffeine neofetch 7zip"
@@ -83,7 +74,7 @@ input_check $@
 sudo apt-get update
 
 if [[ "$evolve" -eq 1 ]]; then
-  maestro_evolve $@
+  maestro_evolve 
 else
   for package in $list; do
     sudo apt-get -y install $package
